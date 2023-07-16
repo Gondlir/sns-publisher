63acb026cd5fcf9d1b29ed61e8dde63e710e43c4
Kaoe Ferreira <kaoeskt@hotmail.com>
2023-07-16 19:08:42 -0300

    first commit and created the project

---
+++ Sns.Publisher/Contracts/CustomerCreated.cs
@@ -0,0 +1,10 @@
+﻿namespace Sns.Publisher.Contracts
+{
+    public sealed class CustomerCreated 
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string Email { get; init; }
+        public string GitHubUsername { get; init; }
+    }
+}
